f21ba08fc817761b84410338b55c1935ce2c5756
paHa345 <pav.345@mail.ru>
2024-11-19 17:13:50 +0700

    Initial Commit

--- app/api/crossword/[crosswordId]/route.tsx
+++ app/api/crossword/[crosswordId]/route.tsx
@@ -34,36 +34,36 @@ import Crossword from "@/app/models/CrosswordModel";
 //   }
 // }
 
-export async function DELETE(req: NextRequest, { params }: { params: { exerciseId: string } }) {
-  const session = await getServerSession(authOptions);
-  if (!session) {
-    return NextResponse.json(
-      { message: "Только для зарегистрированных пользователей" },
-      { status: 401 }
-    );
-  }
+// export async function DELETE(req: NextRequest, { params }: { params: { exerciseId: string } }) {
+//   const session = await getServerSession(authOptions);
+//   if (!session) {
+//     return NextResponse.json(
+//       { message: "Только для зарегистрированных пользователей" },
+//       { status: 401 }
+//     );
+//   }
 
-  try {
-    await connectMongoDB();
-    console.log(params?.exerciseId);
-    const currentUser: IUser | null = await User.findOne({ email: session.user?.email });
-    const currentExercise: IExercise | null = await Exercise.findById(params?.exerciseId);
+//   try {
+//     await connectMongoDB();
+//     console.log(params?.exerciseId);
+//     const currentUser: IUser | null = await User.findOne({ email: session.user?.email });
+//     const currentExercise: IExercise | null = await Exercise.findById(params?.exerciseId);
 
-    if (!currentExercise) {
-      return NextResponse.json({ message: "Упражнение не найдено" }, { status: 404 });
-    }
+//     if (!currentExercise) {
+//       return NextResponse.json({ message: "Упражнение не найдено" }, { status: 404 });
+//     }
 
-    if (String(currentExercise?.createdUserId) !== String(currentUser?._id)) {
-      throw new Error("У вас нет прав для удаления этой трнировки");
-    }
-    const exercise: any = await Exercise.findByIdAndDelete(params?.exerciseId);
-    // if (!exercise) {
-    //   return NextResponse.json({ message: "Упражнение не найдено" }, { status: 404 });
-    // }
-    console.log(exercise);
+//     if (String(currentExercise?.createdUserId) !== String(currentUser?._id)) {
+//       throw new Error("У вас нет прав для удаления этой трнировки");
+//     }
+//     const exercise: any = await Exercise.findByIdAndDelete(params?.exerciseId);
+//     // if (!exercise) {
+//     //   return NextResponse.json({ message: "Упражнение не найдено" }, { status: 404 });
+//     // }
+//     console.log(exercise);
 
-    return NextResponse.json({ message: "Success", result: exercise });
-  } catch (error: any) {
-    return NextResponse.json({ message: error.message }, { status: 400 });
-  }
-}
+//     return NextResponse.json({ message: "Success", result: exercise });
+//   } catch (error: any) {
+//     return NextResponse.json({ message: error.message }, { status: 400 });
+//   }
+// }
