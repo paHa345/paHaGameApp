3c11cedd8a44acc4ea5868e7ac26489befd49703
paHa345 <pav.345@mail.ru>
2024-11-20 11:08:38 +0700

    fix /my prerender

--- app/my/page.tsx
+++ app/my/page.tsx
@@ -8,36 +8,14 @@ import Link from "next/link";
 const My = async () => {
   const session = await getServerSession(authOptions);
 
-  if (!session) {
-    redirect("/");
-    // return (
-    //   <div className="flex flex-col items-center my-32">
-    //     <h1 className=" text-2xl my-5">Вы не зарегистрированы</h1>
-    //     <Link
-    //       className=" text-2xl my-5 hover:text-sky-700 hover:underline hover:underline-offset-4"
-    //       href={`./login`}
-    //     >
-    //       Войти на сайт
-    //     </Link>
-    //   </div>
-    // );
-  }
+  // if (!session) {
+  //   redirect("/");
+  // }
 
   return (
     <>
       <Suspense fallback={"Загрузка..."}>
-        {!session && (
-          <div>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-          </div>
-        )}
-        {session && <MyPage></MyPage>}
+        <MyPage></MyPage>
       </Suspense>
     </>
   );
