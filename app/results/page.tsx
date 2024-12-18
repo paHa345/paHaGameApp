28811bf10f953b3337192bd3223e53233b9f0656
paHa345 <pav.345@mail.ru>
2024-12-18 11:34:28 +0700

    add result page suspense

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ResultsSectionMain from "../components/ResultsSection/ResultsSectionMain";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 const page = () => {
-  return <ResultsSectionMain></ResultsSectionMain>;
+  return (
+    <>
+      {" "}
+      <Suspense
+        fallback={
+          <>
+            <div className=" min-h-[70vh] py-6">
+              <div className="flex justify-center items-center">
+                <div className=" pt-10 flex flex-col justify-center items-center">
+                  <h1 className=" py-5 text-2xl text-center font-bold">Страница загружается ...</h1>
+                  <FontAwesomeIcon className=" animate-spin fa-fw fa-2x" icon={faSpinner} />
+                </div>
+              </div>
+            </div>
+          </>
+        }
+      >
+        <ResultsSectionMain></ResultsSectionMain>
+      </Suspense>{" "}
+    </>
+  );
 };
 
 export default page;
