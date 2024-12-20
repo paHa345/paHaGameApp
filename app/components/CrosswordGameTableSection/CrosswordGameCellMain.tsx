4e1ce6229b1663c33bca5695cdaccd7b8687f426
paHa345 <pav.345@mail.ru>
2024-12-20 15:45:52 +0700

    fix cell input  position

--- app/components/CrosswordGameTableSection/CrosswordGameCellMain.tsx
+++ app/components/CrosswordGameTableSection/CrosswordGameCellMain.tsx
@@ -187,7 +187,7 @@ const CrosswordGameCellMain = ({ refSecound, ref, cell, i, j }: ICellProps) => {
       className={`${isHighlightedWord ? "" : ""} ${!hasLetter ? " bg-slate-100" : "bg-lime-500"} ${isSelectedCell ? "animate-pulse" : ""} transition duration-800 ease-out  cursor-zoom-in   flex gap-1 items-center justify-center h-10 w-10 border-solid border-2 border-slate-600`}
     >
       {hasNumber && (
-        <div className=" relative">
+        <div className=" absolute">
           <p style={{ right: "10px", bottom: "8px" }} className=" relative text-xl font-extrabold">
             {cell.inputValue}
           </p>{" "}
--- app/components/CrosswordGameTableSection/CrosswordGameTableMain.tsx
+++ app/components/CrosswordGameTableSection/CrosswordGameTableMain.tsx
@@ -161,7 +161,7 @@ const CrosswordGameTableMain = () => {
         <EndGameButton></EndGameButton>
         {showEndGameModal && <EndGameModalMain></EndGameModalMain>}
       </div>
-      <div className=" min-w-full overflow-y-scroll">{crosswordGameTableEl}</div>
+      <div className=" min-w-full overflow-x-scroll overflow-y-hidden">{crosswordGameTableEl}</div>
       {startGame && (
         <div className=" pb-20">
           <CrosswordGameQuestionsMain></CrosswordGameQuestionsMain>
