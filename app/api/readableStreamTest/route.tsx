ea679f7f23a3ed73dba43458567451bcdea5373a
paHa345 <pav.345@mail.ru>
2025-02-06 10:55:33 +0700

    add readable stream test v2

--- app/api/readableStreamTest/route.tsx
+++ app/api/readableStreamTest/route.tsx
@@ -9,15 +9,15 @@ export async function GET() {
 
   const readable = new ReadableStream({
     async start(controller) {
-      controller.enqueue(encoder.encode("<html><body>"));
-      await delay(1500);
-      controller.enqueue(encoder.encode("<ul><li>List Item 1</li>"));
-      await delay(4500);
-      controller.enqueue(encoder.encode("<li>List Item 2</li>"));
-      await delay(6500);
-      controller.enqueue(encoder.encode("<li>List Item 3</li></ul>"));
-      await delay(8500);
-      controller.enqueue(encoder.encode("</body></html>"));
+      controller.enqueue(encoder.encode(`${String(Date.now())}  `));
+      await delay(1000);
+      controller.enqueue(encoder.encode(`${String(Date.now())}  `));
+      await delay(1000);
+      controller.enqueue(encoder.encode(`${String(Date.now())}  `));
+      await delay(1000);
+      controller.enqueue(encoder.encode(`${String(Date.now())}  `));
+      await delay(1000);
+      controller.enqueue(encoder.encode(`${String(Date.now())}  `));
       controller.close();
     },
   });
--- app/components/WSTestSection/WSTestMain.tsx
+++ app/components/WSTestSection/WSTestMain.tsx
@@ -60,6 +60,55 @@ const WSTestMain = () => {
     console.log(newEventSource);
     newEventSource.close();
   };
+
+  const startReadableStreamHandler = async () => {
+    console.log("Start Readable Stream");
+    const readable = new ReadableStream({
+      start(controller) {
+        controller.enqueue("Hello, Readable Stream!");
+        controller.enqueue("World!");
+        controller.close();
+      },
+    });
+
+    // const resData = await fetch("/api/readableStreamTest");
+    // const data = await resData.json();
+    // console.log(data);
+
+    async function readData(url: string) {
+      const response = await fetch(url);
+
+      if (response.body) {
+        const data: any = response.body;
+        for await (const chunk of data) {
+          // Do something with each "chunk"
+          console.log(new TextDecoder().decode(chunk));
+        }
+        // Exit when done
+      }
+    }
+
+    readData("/api/readableStreamTest");
+
+    // await fetch("/api/readableStreamTest")
+    //   .then((response) => response.text()) // Получаем текстовую информацию
+    //   .then((text) => console.log(text))
+    //   .catch((error) => console.error("Произошла ошибка:", error));
+
+    // if (resData.body) {
+    //   const reader = resData.body.getReader();
+    //   console.log(reader);
+    // }
+    // (async () => {
+    //   while (true) {
+    //     const { done, value } = await reader.read();
+    //     if (done) {
+    //       break;
+    //     }
+    //     console.log(value);
+    //   }
+    // })();
+  };
   return (
     <div>
       {" "}
@@ -77,6 +126,14 @@ const WSTestMain = () => {
           onClick={stopSSEHandler.bind("stop")}
         >
           <h1>Stop</h1>
+        </div>
+      </div>
+      <div className=" flex justify-center items-center ">
+        <div
+          className={` cursor-pointer font-bold text-2xl  bg-lime-300 mx-2 my-2 py-4 px-4 rounded-lg hover:scale-105 hover:text-slate-500`}
+          onClick={startReadableStreamHandler}
+        >
+          <h1>Start Readable Stream</h1>
         </div>
       </div>
     </div>
