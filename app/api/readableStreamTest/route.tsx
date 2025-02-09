f9de5fa0db63a194bf1ff692aa1b6ab4710686ad
paHa345 <pav.345@mail.ru>
2025-02-09 14:36:27 +0700

    add consolle req signal

--- app/api/readableStreamTest/route.tsx
+++ app/api/readableStreamTest/route.tsx
@@ -1,6 +1,6 @@
 import { connectMongoDB } from "@/app/libs/MongoConnect";
 import GTSGameAttempt from "@/app/models/GTSGameAttemptModel";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
   runtime: "edge",
@@ -10,76 +10,92 @@ export const config = {
 // export const maxDuration = 20;
 
 export async function GET(req: NextRequest) {
-  const encoder = new TextEncoder();
-  await connectMongoDB();
+  let aborted = false;
 
-  const readable = new ReadableStream({
-    async start(controller) {
-      const intervalID = setInterval(async () => {
-        // const currentGTSGameAttemptTime = await GTSGameAttempt.findById(
-        //   "679affc8d6353d1c90440870"
-        // ).select("timeRemained");
+  req.signal.onabort = () => {
+    console.log("Request aborted");
+    aborted = true;
+    // cleanup()
+  };
 
-        // const currentTime = JSON.parse(JSON.stringify(currentGTSGameAttemptTime.timeRemained));
-        // const updatedGTSGameAttempt = await GTSGameAttempt.findByIdAndUpdate(
-        //   "679affc8d6353d1c90440870",
-        //   {
-        //     $set: { timeRemained: currentTime - 10 },
-        //   }
-        // );
-        // console.log(updatedGTSGameAttempt.timeRemained);
-        // console.log(readable);
-        // if (readable.locked) {
-        //   controller.enqueue(encoder.encode(`${String(Date.now())}  `));
-        // }
-        fetchData();
-      }, 1000);
+  try {
+    const encoder = new TextEncoder();
+    await connectMongoDB();
 
-      async function fetchData() {
-        if (readable.locked) {
-          const currentGTSGameAttemptTime = await GTSGameAttempt.findById(
-            "679affc8d6353d1c90440870"
-          ).select("timeRemained");
+    const readable = new ReadableStream({
+      async start(controller) {
+        const intervalID = setInterval(async () => {
+          // const currentGTSGameAttemptTime = await GTSGameAttempt.findById(
+          //   "679affc8d6353d1c90440870"
+          // ).select("timeRemained");
 
-          const currentTime = JSON.parse(
-            JSON.stringify(currentGTSGameAttemptTime.timeRemained)
-          );
-          const updatedGTSGameAttempt = await GTSGameAttempt.findByIdAndUpdate(
-            "679affc8d6353d1c90440870",
-            {
-              $set: { timeRemained: currentTime - 10 },
-            },
-            {
-              new: true,
-            }
-          ).select("timeRemained");
-          console.log(updatedGTSGameAttempt.timeRemained);
-          console.log(req.signal.onabort);
+          // const currentTime = JSON.parse(JSON.stringify(currentGTSGameAttemptTime.timeRemained));
+          // const updatedGTSGameAttempt = await GTSGameAttempt.findByIdAndUpdate(
+          //   "679affc8d6353d1c90440870",
+          //   {
+          //     $set: { timeRemained: currentTime - 10 },
+          //   }
+          // );
+          // console.log(updatedGTSGameAttempt.timeRemained);
+          // console.log(readable);
+          // if (readable.locked) {
+          //   controller.enqueue(encoder.encode(`${String(Date.now())}  `));
+          // }
+          fetchData();
+        }, 1000);
 
-          controller.enqueue(
-            encoder.encode(`${String(updatedGTSGameAttempt.timeRemained)}  `)
-          );
-        } else {
-          clearInterval(intervalID);
-          controller.close();
+        async function fetchData() {
+          if (readable.locked) {
+            const currentGTSGameAttemptTime = await GTSGameAttempt.findById(
+              "679affc8d6353d1c90440870"
+            ).select("timeRemained");
+
+            const currentTime = JSON.parse(
+              JSON.stringify(currentGTSGameAttemptTime.timeRemained)
+            );
+            const updatedGTSGameAttempt =
+              await GTSGameAttempt.findByIdAndUpdate(
+                "679affc8d6353d1c90440870",
+                {
+                  $set: { timeRemained: currentTime - 10 },
+                },
+                {
+                  new: true,
+                }
+              ).select("timeRemained");
+            console.log(updatedGTSGameAttempt.timeRemained);
+            console.log(req.signal);
+
+            controller.enqueue(
+              encoder.encode(`${String(updatedGTSGameAttempt.timeRemained)}  `)
+            );
+          } else {
+            clearInterval(intervalID);
+            controller.close();
+          }
         }
-      }
 
-      // controller.enqueue(encoder.encode(`${String(Date.now())}  `));
-      // await delay(1000);
+        // controller.enqueue(encoder.encode(`${String(Date.now())}  `));
+        // await delay(1000);
 
-      // controller.enqueue(encoder.encode(`${String(Date.now())}  `));
-      // await delay(1000);
-      // controller.enqueue(encoder.encode(`${String(Date.now())}  `));
-      // await delay(1000);
-      // controller.enqueue(encoder.encode(`${String(Date.now())}  `));
-      // await delay(1000);
-      // controller.enqueue(encoder.encode(`${String(Date.now())}  `));
-      // controller.close();
-    },
-  });
+        // controller.enqueue(encoder.encode(`${String(Date.now())}  `));
+        // await delay(1000);
+        // controller.enqueue(encoder.encode(`${String(Date.now())}  `));
+        // await delay(1000);
+        // controller.enqueue(encoder.encode(`${String(Date.now())}  `));
+        // await delay(1000);
+        // controller.enqueue(encoder.encode(`${String(Date.now())}  `));
+        // controller.close();
+      },
+    });
 
-  return new Response(readable, {
-    headers: { "Content-Type": "text/html; charset=utf-8" },
-  });
+    return new Response(readable, {
+      headers: { "Content-Type": "text/html; charset=utf-8" },
+    });
+  } catch (error: any) {
+    return NextResponse.json(
+      { message: "Request was aborted" },
+      { status: 499 }
+    );
+  }
 }
