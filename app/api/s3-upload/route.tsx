fc705a3395c9d416430ab85a067ab43b977bae9f
paHa345 <pav.345@mail.ru>
2025-03-24 11:06:16 +0700

    add s3 timeweb cloud example v2

--- app/api/s3-upload/route.tsx
+++
@@ -1,7 +0,0 @@
-// import { POST as APIRoute } from "next-s3-upload/route";
-
-// export const POST = APIRoute.configure({
-//   bucket: "f1525e96-2c5a759f-3888-4bd2-a52f-dbb62685b4bb",
-//   region: "ru-1",
-//   endpoint: "https://nyc3.digitaloceanspaces.com",
-// });
