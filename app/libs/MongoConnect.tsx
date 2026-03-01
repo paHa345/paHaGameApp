56f056fe7cdea9c75bddcc2adccdd4e69e7bcf53
paHa345 <pav.345@mail.ru>
2026-03-01 22:20:08 +0700

    change db to vercel

--- app/libs/MongoConnect.tsx
+++ app/libs/MongoConnect.tsx
@@ -6,8 +6,9 @@ export const connectMongoDB = async () => {
     return mongoose.connection.asPromise();
   }
   return await mongoose.connect(
-    `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@95.140.157.193:27017/paHaGTSGameDB?authSource=admin&directConnection=true`
+    // `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@95.140.157.193:27017/paHaGTSGameDB?authSource=admin&directConnection=true`
 
+    `mongodb+srv://Vercel-Admin-${process.env.MONGODB_USERNAME}-VercelDB:${process.env.MONGODB_PASSWORD}@paha345-verceldb.z4hjuwq.mongodb.net/paHaGameDB?retryWrites=true&w=majority`,
     // `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bnjpnqkq0agsple?replicaSet=rs0`
   );
 };
