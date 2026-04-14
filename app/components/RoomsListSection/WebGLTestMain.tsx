0b76c192b6806848b4e753c976d0fad7e7c7507a
paHa345 <pav.345@mail.ru>
2026-04-14 14:13:09 +0700

    add three js example 1.20 water shaders

--- app/components/RoomsListSection/WebGLTestMain.tsx
+++ app/components/RoomsListSection/WebGLTestMain.tsx
@@ -23,6 +23,8 @@ import Robot from "./Robot";
 import Experience from "./Experience/Experience";
 import testVertexShaders from "./shaders/test/vertex.glsl";
 import testFragmentShaders from "./shaders/test/fragment.glsl";
+import waterVertexShader from "./shaders/water/vertex.glsl";
+import waterFragmentShader from "./shaders/water/fragment.glsl";
 
 const WebGLTestMain = () => {
   const GLCanvasRef = useRef<HTMLCanvasElement>(null);
@@ -100,6 +102,11 @@ const WebGLTestMain = () => {
       // Debug
       const gui = new GUI();
 
+      const debugObject: {
+        depthColor?: string;
+        surfaceColor?: string;
+      } = {};
+
       // Scene
       const scene = new THREE.Scene();
 
@@ -110,53 +117,127 @@ const WebGLTestMain = () => {
       const flagTexture = textureLoader.load("/textures/Russia.jpg");
 
       /**
-       * Test mesh
+       * Water
        */
       // Geometry
-      const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
+      const waterGeometry = new THREE.PlaneGeometry(2, 2, 512, 512);
 
-      console.log(geometry);
+      // Color
 
-      const count = geometry.attributes.position.count;
-
-      const randoms = new Float32Array(count);
-
-      for (let i = 0; i < count; i++) {
-        randoms[i] = Math.random();
-      }
-
-      geometry.setAttribute("aRandom", new THREE.BufferAttribute(randoms, 1));
+      debugObject.depthColor = "#186691";
+      debugObject.surfaceColor = "#9bd8ff";
 
       // Material
-      const material = new THREE.ShaderMaterial({
-        vertexShader: testVertexShaders,
-        fragmentShader: testFragmentShaders,
-        side: THREE.DoubleSide,
+      const waterMaterial = new THREE.ShaderMaterial({
+        vertexShader: waterVertexShader,
+        fragmentShader: waterFragmentShader,
+        // wireframe: true,
         uniforms: {
-          uFraquency: { value: new THREE.Vector2(10, 5) },
           uTime: { value: 0 },
-          uColor: { value: new THREE.Color("orange") },
-          uTexture: { value: flagTexture },
+
+          uBigWaveSpeed: { value: 0.5 },
+          uBigWavesElevation: { value: 0.2 },
+          uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
+
+          uSmallWavesElevation: { value: 0.15 },
+          uSmallWavesFrequency: { value: 3 },
+          uSmallWavesSpeed: { value: 0.2 },
+          uSmallWavesIterations: { value: 4 },
+
+          uDepthColor: { value: new THREE.Color(debugObject.depthColor) },
+          uSurfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
+          uColorOffset: { value: 0.08 },
+          uColorMultiplier: { value: 5 },
         },
       });
 
+      // Debug
+
       gui
-        .add(material.uniforms.uFraquency.value, "x")
+        .add(waterMaterial.uniforms.uBigWavesElevation, "value")
         .min(0)
-        .max(20)
-        .step(0.01)
-        .name("Частота по оси X");
+        .max(1)
+        .step(0.001)
+        .name("Высота больших волн");
+
       gui
-        .add(material.uniforms.uFraquency.value, "y")
+        .add(waterMaterial.uniforms.uBigWaveSpeed, "value")
         .min(0)
-        .max(20)
+        .max(4)
+        .step(0.001)
+        .name("Скорость больших волн");
+
+      gui
+        .add(waterMaterial.uniforms.uBigWavesFrequency.value, "x")
+        .min(0)
+        .max(10)
+        .step(0.001)
+        .name("Частота больших волн по X");
+
+      gui
+        .add(waterMaterial.uniforms.uBigWavesFrequency.value, "y")
+        .min(0)
+        .max(10)
+        .step(0.001)
+        .name("Частота больших волн по Y");
+
+      gui
+        .add(waterMaterial.uniforms.uSmallWavesElevation, "value")
+        .min(0)
+        .max(1)
+        .step(0.001)
+        .name("Высота малых волн");
+      gui
+        .add(waterMaterial.uniforms.uSmallWavesFrequency, "value")
+        .min(0)
+        .max(30)
+        .step(0.001)
+        .name("Частота малых волн");
+      gui
+        .add(waterMaterial.uniforms.uSmallWavesSpeed, "value")
+        .min(0)
+        .max(4)
         .step(0.01)
-        .name("Частота по оси Y");
+        .name("Скорость малых волн");
+      gui
+        .add(waterMaterial.uniforms.uSmallWavesIterations, "value")
+        .min(0)
+        .max(4)
+        .step(1)
+        .name("Количество малых волн");
+
+      gui
+        .addColor(debugObject, "depthColor")
+        .name("Цвет глубины")
+        .onChange(() => {
+          waterMaterial.uniforms.uDepthColor.value.set(debugObject.depthColor);
+        });
+
+      gui
+        .addColor(debugObject, "surfaceColor")
+        .name("Цвет поверхности")
+        .onChange(() => {
+          waterMaterial.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor);
+        });
+
+      gui
+        .add(waterMaterial.uniforms.uColorOffset, "value")
+        .min(0)
+        .max(1)
+        .step(0.001)
+        .name("Смещение цвета");
+
+      gui
+        .add(waterMaterial.uniforms.uColorMultiplier, "value")
+        .min(0)
+        .max(10)
+        .step(0.001)
+        .name("Множитель цвета");
 
       // Mesh
-      const mesh = new THREE.Mesh(geometry, material);
-      mesh.scale.y = 2 / 3;
-      scene.add(mesh);
+      const water = new THREE.Mesh(waterGeometry, waterMaterial);
+      water.rotation.x = -Math.PI * 0.5;
+      scene.add(water);
 
       window.addEventListener("resize", () => {
         // Update sizes
@@ -177,7 +258,7 @@ const WebGLTestMain = () => {
        */
       // Base camera
       const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-      camera.position.set(0.25, -0.25, 1);
+      camera.position.set(1, 1, 1);
       scene.add(camera);
 
       // Controls
@@ -212,12 +293,9 @@ const WebGLTestMain = () => {
 
         const elapsedTime = timer.getElapsed();
 
-        const deltaTime = elapsedTime - previousTime;
-        previousTime = elapsedTime;
+        // Update water
 
-        // Update material
-
-        material.uniforms.uTime.value = elapsedTime;
+        waterMaterial.uniforms.uTime.value = elapsedTime;
 
         // Update controls
         controls.update();
--- app/components/RoomsListSection/shaders/test/vertex.glsl
+++ app/components/RoomsListSection/shaders/test/vertex.glsl
@@ -1,46 +1,8 @@
-// uniform mat4 projectionMatrix;
-// uniform mat4 viewMatrix;
-// uniform mat4 modelMatrix;
-uniform vec2 uFraquency;
-uniform float uTime;
-
-
-// attribute vec3 position;
-// attribute float aRandom;
-// attribute vec2 uv;
-
-// varying float vRandom;
 varying vec2 vUv;
-varying float vElevation;
 
-
-void main(){
-    
-
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-
-    float elevation = sin(modelPosition.x * uFraquency.x - uTime)*0.1;
-    elevation+=sin(modelPosition.y * uFraquency.y - uTime)*0.1;
-
-    modelPosition.z += elevation;
-
-    // modelPosition.z += sin(modelPosition.x * uFraquency.x + uTime)*0.1;
-    // modelPosition.z += sin(modelPosition.y * uFraquency.y - uTime)*0.1;
-
-
-
-    // modelPosition.z += aRandom * 0.05;
-
-    vec4 viewPosition = viewMatrix * modelPosition;
-    vec4 projectedPosition = projectionMatrix * viewPosition;
-
-    gl_Position = projectedPosition;
+void main()
+{
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 
     vUv = uv;
-    
-    vElevation = elevation;
-    // vRandom = aRandom;
-
-// gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
-
 }
