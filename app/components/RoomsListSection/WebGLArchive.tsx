54625eec2a5d572e609633d8eb254fd722af6ec4
paHa345 <pav.345@mail.ru>
2026-05-06 16:14:23 +0700

    add three js example 1.42 wobble shader

--- app/components/RoomsListSection/WebGLArchive.tsx
+++ app/components/RoomsListSection/WebGLArchive.tsx
@@ -6172,3 +6172,317 @@
 //       // return () => window.removeEventListener("resize", handleResize);
 //     }
 //   });
+
+/**
+ * 41 GPGPU Flow field particles shader
+ */
+
+// vertex and fragment shader from /shaders/40_gpgpuParticles
+// particles shaders from /shaders/gpgpu/
+
+//   useEffect(() => {
+//     if (typeof window !== "undefined") {
+//       // const expirience = new Experience(GLCanvasRef.current);
+
+//       const sizes = {
+//         width: 800,
+//         height: 600,
+//         resolution: new THREE.Vector2(800, 600),
+//         pixelRatio: Math.min(window.devicePixelRatio, 2),
+//       };
+
+//       // sizes.resolution = new THREE.Vector2(sizes.width, sizes.height);
+
+//       /**
+//        * Base
+//        */
+//       // Debug
+//       const gui = new GUI();
+
+//       // Scene
+//       const scene = new THREE.Scene();
+
+//       // Loaders
+//       const dracoLoader = new DRACOLoader();
+//       dracoLoader.setDecoderPath("./draco/");
+//       const gltfLoader = new GLTFLoader();
+//       gltfLoader.setDRACOLoader(dracoLoader);
+
+//       window.addEventListener("resize", () => {
+//         // Update sizes
+//         sizes.width = window.innerWidth;
+//         sizes.height = window.innerHeight;
+//         sizes.pixelRatio = Math.min(window.devicePixelRatio, 2);
+
+//         sizes.resolution.set(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio);
+
+//         // Materials
+//         if (particles) {
+//           particles.material.uniforms.uResolution.value.set(
+//             sizes.width * sizes.pixelRatio,
+//             sizes.height * sizes.pixelRatio,
+//           );
+//         }
+
+//         // Update camera
+//         camera.aspect = sizes.width / sizes.height;
+//         camera.updateProjectionMatrix();
+
+//         // Update renderer
+//         renderer.toneMapping = THREE.ACESFilmicToneMapping;
+//         renderer.setSize(sizes.width, sizes.height);
+//         renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+//       });
+
+//       /**
+//        * Camera
+//        */
+//       // Base camera
+//       const camera = new THREE.PerspectiveCamera(25, sizes.width / sizes.height, 0.1, 100);
+//       camera.position.set(4.5, 4, 11);
+
+//       scene.add(camera);
+
+//       // Controls
+//       const controls = new OrbitControls(camera, GLCanvasRef.current);
+//       controls.enableDamping = true;
+
+//       const debugObject = {
+//         clearColor: "#160920",
+//       };
+
+//       /**
+//        * Renderer
+//        */
+
+//       if (GLCanvasRef.current === null) {
+//         return;
+//       }
+//       const renderer = new THREE.WebGLRenderer({
+//         canvas: GLCanvasRef.current,
+//         // antialias: true,
+//       });
+//       // renderer.toneMapping = THREE.ACESFilmicToneMapping
+//       // renderer.toneMappingExposure = 3
+//       renderer.setSize(sizes.width, sizes.height);
+//       renderer.setPixelRatio(sizes.pixelRatio);
+//       renderer.setClearColor(debugObject.clearColor);
+
+//       /**
+//        * Load geometry
+//        */
+
+//       const loadModel = async () => {
+//         try {
+//           const gltf = await gltfLoader.loadAsync("./shipModel.glb");
+//           modelRef.current = gltf;
+//         } catch (error) {
+//           console.error("Ошибка загрузки модели:", error);
+//         }
+//       };
+
+//       loadModel();
+
+//       if (modelRef.current === null) return;
+
+//       /**
+//        * Base geometry
+//        */
+
+//       const baseGeometry = {
+//         instance: modelRef.current.scene.children[0].geometry,
+//         count: 0,
+//       };
+
+//       baseGeometry.count = baseGeometry.instance.attributes.position.count;
+
+//       /**
+//        * GPU compute
+//        */
+//       // Setup
+//       const gpgpu: {
+//         size: number;
+//         computation?: GPUComputationRenderer;
+//         particlesVariable?: Variable;
+//         debug?: THREE.Mesh;
+//       } = { size: Math.ceil(Math.sqrt(baseGeometry.count)) };
+//       gpgpu.computation = new GPUComputationRenderer(gpgpu.size, gpgpu.size, renderer);
+
+//       // Base particles
+
+//       const baseParticlesTexture = gpgpu.computation.createTexture();
+
+//       for (let i = 0; i < baseGeometry.count; i++) {
+//         const i3 = i * 3;
+//         const i4 = i * 4;
+
+//         // Position based on geometry
+//         if (!baseParticlesTexture.image.data) return;
+//         baseParticlesTexture.image.data[i4 + 0] =
+//           baseGeometry.instance.attributes.position.array[i3 + 0];
+//         baseParticlesTexture.image.data[i4 + 1] =
+//           baseGeometry.instance.attributes.position.array[i3 + 1];
+//         baseParticlesTexture.image.data[i4 + 2] =
+//           baseGeometry.instance.attributes.position.array[i3 + 2];
+//         baseParticlesTexture.image.data[i4 + 3] = Math.random();
+//       }
+
+//       // Particles variable
+//       gpgpu.particlesVariable = gpgpu.computation.addVariable(
+//         "uParticles",
+//         gpgpuParticlesShader,
+//         baseParticlesTexture,
+//       );
+//       gpgpu.computation.setVariableDependencies(gpgpu.particlesVariable, [gpgpu.particlesVariable]);
+
+//       // Uniforms
+
+//       gpgpu.particlesVariable.material.uniforms.uTime = new THREE.Uniform(0);
+//       gpgpu.particlesVariable.material.uniforms.uDeltaTime = new THREE.Uniform(0);
+//       gpgpu.particlesVariable.material.uniforms.uBase = new THREE.Uniform(baseParticlesTexture);
+//       gpgpu.particlesVariable.material.uniforms.uFlowFieldInfluence = new THREE.Uniform(0.5);
+//       gpgpu.particlesVariable.material.uniforms.uFlowFieldStrength = new THREE.Uniform(2);
+//       gpgpu.particlesVariable.material.uniforms.uFlowFieldFrequency = new THREE.Uniform(0.5);
+
+//       // Init
+//       gpgpu.computation.init();
+
+//       // Debug
+
+//       gpgpu.debug = new THREE.Mesh(
+//         new THREE.PlaneGeometry(3, 3),
+//         new THREE.MeshBasicMaterial({
+//           map: gpgpu.computation.getAlternateRenderTarget(gpgpu.particlesVariable).texture,
+//         }),
+//       );
+
+//       gpgpu.debug.visible = false;
+//       gpgpu.debug.position.x = 3;
+
+//       scene.add(gpgpu.debug);
+
+//       /**
+//        * Particles
+//        */
+
+//       const particles: {
+//         material: THREE.ShaderMaterial;
+//         points?: THREE.Points;
+//         geometry?: THREE.BufferGeometry;
+//       } = {
+//         material: new THREE.ShaderMaterial({
+//           vertexShader: particlesVertexShader,
+//           fragmentShader: particlesFragmentShader,
+//           uniforms: {
+//             uSize: new THREE.Uniform(0.07),
+//             uResolution: new THREE.Uniform(
+//               new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio),
+//             ),
+//             uParticlesTexture: new THREE.Uniform(1),
+//           },
+//         }),
+//       };
+
+//       // Geometry
+//       const particlesUVArray = new Float32Array(baseGeometry.count * 2);
+//       const sizesArray = new Float32Array(baseGeometry.count);
+
+//       for (let y = 0; y < gpgpu.size; y++) {
+//         for (let x = 0; x < gpgpu.size; x++) {
+//           const i = y * gpgpu.size + x;
+//           const i2 = i * 2;
+
+//           const uvX = (x + 0.5) / gpgpu.size;
+//           const uvY = (y + 0.5) / gpgpu.size;
+//           particlesUVArray[i2 + 0] = uvX;
+//           particlesUVArray[i2 + 1] = uvY;
+
+//           sizesArray[i] = Math.random();
+//         }
+//       }
+
+//       particles.geometry = new THREE.BufferGeometry();
+//       particles.geometry.setDrawRange(0, baseGeometry.count);
+//       particles.geometry.setAttribute(
+//         "aParticlesUv",
+//         new THREE.BufferAttribute(particlesUVArray, 2),
+//       );
+//       particles.geometry.setAttribute("aColor", baseGeometry.instance.attributes.color);
+//       particles.geometry.setAttribute("aSize", new THREE.BufferAttribute(sizesArray, 1));
+
+//       // Points
+//       particles.points = new THREE.Points(particles.geometry, particles.material);
+//       scene.add(particles.points);
+
+//       /**
+//        * Tweaks
+//        */
+//       gui.addColor(debugObject, "clearColor").onChange(() => {
+//         renderer.setClearColor(debugObject.clearColor);
+//       });
+//       gui.add(particles.material.uniforms.uSize, "value").min(0).max(1).step(0.001).name("uSize");
+
+//       gui
+//         .add(gpgpu.particlesVariable.material.uniforms.uFlowFieldInfluence, "value")
+//         .min(0)
+//         .max(1)
+//         .name("Влияние потока");
+
+//       gui
+//         .add(gpgpu.particlesVariable.material.uniforms.uFlowFieldStrength, "value")
+//         .min(0)
+//         .max(10)
+//         .name("Сила потока");
+//       gui
+//         .add(gpgpu.particlesVariable.material.uniforms.uFlowFieldFrequency, "value")
+//         .min(0)
+//         .max(1)
+//         .step(0.001)
+//         .name("Частота потока");
+
+//       /**
+//        * Animate
+//        */
+
+//       const timer = new THREE.Timer();
+//       let previousTime = 0;
+
+//       let currentIntersect: null | THREE.Intersection<THREE.Object3D<THREE.Object3DEventMap>> =
+//         null;
+
+//       const tick = () => {
+//         // controls.update();
+//         timer.update();
+
+//         const elapsedTime = timer.getElapsed();
+//         const deltaTime = elapsedTime - previousTime;
+//         previousTime = elapsedTime;
+
+//         // Update controls
+//         controls.update();
+
+//         // GPGPU Update
+//         if (!gpgpu.computation) return;
+//         if (!gpgpu.particlesVariable) return;
+
+//         gpgpu.particlesVariable.material.uniforms.uTime.value = elapsedTime;
+//         gpgpu.particlesVariable.material.uniforms.uDeltaTime.value = deltaTime;
+
+//         gpgpu.computation?.compute();
+
+//         particles.material.uniforms.uParticlesTexture.value =
+//           gpgpu.computation.getCurrentRenderTarget(gpgpu.particlesVariable).texture;
+
+//         // Render
+//         renderer.render(scene, camera);
+
+//         // Call tick again on the next frame
+//         window.requestAnimationFrame(tick);
+//       };
+
+//       tick();
+
+//       // Cleanup
+//       // return () => window.removeEventListener("resize", handleResize);
+//     }
+//   });
--- app/components/RoomsListSection/WebGLTestMain.tsx
+++ app/components/RoomsListSection/WebGLTestMain.tsx
@@ -18,10 +18,7 @@ import * as CANNON from "cannon-es";
 import { GLTF, GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
 import { paginateListObjectsV2 } from "@aws-sdk/client-s3";
-import {
-  GPUComputationRenderer,
-  Variable,
-} from "three/addons/misc/GPUComputationRenderer.js";
+import { GPUComputationRenderer, Variable } from "three/addons/misc/GPUComputationRenderer.js";
 
 // import FlyingRobot from "./FlyingRobot";
 // import Robot from "./Robot";
@@ -50,10 +47,15 @@ import {
 // import atmosphereFragmentShader from "./shaders/atmosphere/fragment.glsl";
 // import particlesMorphVertexShader from "./shaders/morphParticles/vertex.glsl";
 // import particlesMorphFragmentShader from "./shaders/morphParticles/fragment.glsl";
+import { mergeVertices } from "three/addons/utils/BufferGeometryUtils.js";
 
-import particlesVertexShader from "./shaders/40_gpgpuParticles/vertex.glsl";
-import particlesFragmentShader from "./shaders/40_gpgpuParticles/fragment.glsl";
-import gpgpuParticlesShader from "./shaders/gpgpu/particles.glsl";
+import CustomShaderMaterial from "three-custom-shader-material/vanilla";
+
+// import particlesVertexShader from "./shaders/40_gpgpuParticles/vertex.glsl";
+// import particlesFragmentShader from "./shaders/40_gpgpuParticles/fragment.glsl";
+// import gpgpuParticlesShader from "./shaders/gpgpu/particles.glsl";
+import wobbleVertexShader from "./shaders/wobble/vertex.glsl";
+import wobbleFragmentShader from "./shaders/wobble/fragment.glsl";
 
 const WebGLTestMain = () => {
   const GLCanvasRef = useRef<HTMLCanvasElement>(null);
@@ -68,6 +70,8 @@ const WebGLTestMain = () => {
   const [cubeColor, setCubeColor] = useState({
     color: 0xff0000,
   });
+
+  const modelRef = useRef<any>(null);
 
   const [fullScreenSTatus, setFullScreenSTatus] = useState(1);
 
@@ -116,10 +120,144 @@ const WebGLTestMain = () => {
       const scene = new THREE.Scene();
 
       // Loaders
+      const rgbeLoader = new HDRLoader();
       const dracoLoader = new DRACOLoader();
       dracoLoader.setDecoderPath("./draco/");
       const gltfLoader = new GLTFLoader();
       gltfLoader.setDRACOLoader(dracoLoader);
+
+      /**
+       * Environment map
+       */
+      rgbeLoader.load("./urban_alley_01_1k.hdr", (environmentMap) => {
+        environmentMap.mapping = THREE.EquirectangularReflectionMapping;
+
+        scene.background = environmentMap;
+        scene.environment = environmentMap;
+      });
+
+      const debugObject = {
+        clearColor: "#160920",
+        colorA: "#0000ff",
+        colorB: "#ff0000",
+      };
+
+      /**
+       * Wobble
+       */
+
+      const uniforms = {
+        uTime: new THREE.Uniform(0),
+        uPositionFrequency: new THREE.Uniform(0.5),
+        uTimeFrequency: new THREE.Uniform(0.4),
+        uStrength: new THREE.Uniform(0.3),
+
+        uWarpPositionFrequency: new THREE.Uniform(0.38),
+        uWarpTimeFrequency: new THREE.Uniform(0.12),
+        uWarpStrength: new THREE.Uniform(1.7),
+
+        uColorA: new THREE.Uniform(new THREE.Color(debugObject.colorA)),
+        uColorB: new THREE.Uniform(new THREE.Color(debugObject.colorB)),
+      };
+
+      // Material
+      const material = new CustomShaderMaterial({
+        // CSM
+        baseMaterial: THREE.MeshPhysicalMaterial,
+        vertexShader: wobbleVertexShader,
+        fragmentShader: wobbleFragmentShader,
+        uniforms: uniforms,
+        // MeshPhysicalMaterial
+        metalness: 0,
+        roughness: 0.5,
+        color: "#ffffff",
+        transmission: 0,
+        ior: 1.5,
+        thickness: 1.5,
+        transparent: true,
+        wireframe: false,
+      });
+
+      const depthMaterial = new CustomShaderMaterial({
+        // CSM
+        baseMaterial: THREE.MeshDepthMaterial,
+        vertexShader: wobbleVertexShader,
+        uniforms: uniforms,
+
+        // MeshDepthMaterial
+        depthPacking: THREE.RGBADepthPacking,
+      });
+
+      // Tweaks
+
+      gui.add(uniforms.uPositionFrequency, "value", 0, 2, 0.001).name("Частота");
+      gui.add(uniforms.uTimeFrequency, "value", 0, 2, 0.001).name("Частота по времени");
+      gui.add(uniforms.uStrength, "value", 0, 2, 0.001).name("Сила");
+
+      gui.add(uniforms.uWarpPositionFrequency, "value", 0, 2, 0.001).name("Частота деформации");
+      gui
+        .add(uniforms.uWarpTimeFrequency, "value", 0, 2, 0.001)
+        .name("Частота деформации по времени");
+      gui.add(uniforms.uWarpStrength, "value", 0, 2, 0.001).name("Сила деформации");
+
+      gui.addColor(debugObject, "colorA").onChange(() => {
+        uniforms.uColorA.value.set(debugObject.colorA);
+      });
+      gui.addColor(debugObject, "colorB").onChange(() => {
+        uniforms.uColorB.value.set(debugObject.colorB);
+      });
+
+      // gui.add(material, "metalness", 0, 1, 0.001);
+      // gui.add(material, "roughness", 0, 1, 0.001);
+      // gui.add(material, "transmission", 0, 1, 0.001);
+      // gui.add(material, "ior", 0, 10, 0.001);
+      // gui.add(material, "thickness", 0, 10, 0.001);
+      // gui.addColor(material, "color");
+
+      // // Geometry
+      // let geometry = new THREE.IcosahedronGeometry(2.5, 50) as any;
+      // geometry = mergeVertices(geometry);
+      // geometry.computeTangents();
+
+      // // Mesh
+      // const wobble = new THREE.Mesh(geometry, material);
+      // wobble.customDepthMaterial = depthMaterial;
+      // wobble.receiveShadow = true;
+      // wobble.castShadow = true;
+      // scene.add(wobble);
+
+      // Model
+      gltfLoader.load("./suzanne.glb", (gltf) => {
+        const wobble = gltf.scene.children[0] as any;
+        wobble.castShadow = true;
+        wobble.material = material;
+        wobble.customDepthMaterial = depthMaterial;
+        scene.add(wobble);
+      });
+
+      /**
+       * Plane
+       */
+      const plane = new THREE.Mesh(
+        new THREE.PlaneGeometry(15, 15, 15),
+        new THREE.MeshStandardMaterial(),
+      );
+      plane.receiveShadow = true;
+      plane.rotation.y = Math.PI;
+      plane.position.y = -5;
+      plane.position.z = 5;
+      scene.add(plane);
+
+      /**
+       * Lights
+       */
+      const directionalLight = new THREE.DirectionalLight("#ffffff", 3);
+      directionalLight.castShadow = true;
+      directionalLight.shadow.mapSize.set(1024, 1024);
+      directionalLight.shadow.camera.far = 15;
+      directionalLight.shadow.normalBias = 0.05;
+      directionalLight.position.set(0.25, 2, -2.25);
+      scene.add(directionalLight);
 
       window.addEventListener("resize", () => {
         // Update sizes
@@ -127,18 +265,15 @@ const WebGLTestMain = () => {
         sizes.height = window.innerHeight;
         sizes.pixelRatio = Math.min(window.devicePixelRatio, 2);
 
-        sizes.resolution.set(
-          sizes.width * sizes.pixelRatio,
-          sizes.height * sizes.pixelRatio,
-        );
+        sizes.resolution.set(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio);
 
-        // Materials
-        if (particles) {
-          particles.material.uniforms.uResolution.value.set(
-            sizes.width * sizes.pixelRatio,
-            sizes.height * sizes.pixelRatio,
-          );
-        }
+        // // Materials
+        // if (particles) {
+        //   particles.material.uniforms.uResolution.value.set(
+        //     sizes.width * sizes.pixelRatio,
+        //     sizes.height * sizes.pixelRatio,
+        //   );
+        // }
 
         // Update camera
         camera.aspect = sizes.width / sizes.height;
@@ -154,23 +289,13 @@ const WebGLTestMain = () => {
        * Camera
        */
       // Base camera
-      const camera = new THREE.PerspectiveCamera(
-        25,
-        sizes.width / sizes.height,
-        0.1,
-        100,
-      );
-      camera.position.set(4.5, 4, 11);
-
+      const camera = new THREE.PerspectiveCamera(25, sizes.width / sizes.height, 0.1, 100);
+      camera.position.set(13, -3, -5);
       scene.add(camera);
 
       // Controls
       const controls = new OrbitControls(camera, GLCanvasRef.current);
       controls.enableDamping = true;
-
-      const debugObject = {
-        clearColor: "#160920",
-      };
 
       /**
        * Renderer
@@ -181,102 +306,14 @@ const WebGLTestMain = () => {
       }
       const renderer = new THREE.WebGLRenderer({
         canvas: GLCanvasRef.current,
-        // antialias: true,
+        antialias: true,
       });
-      // renderer.toneMapping = THREE.ACESFilmicToneMapping
-      // renderer.toneMappingExposure = 3
+      renderer.shadowMap.enabled = true;
+      renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+      renderer.toneMapping = THREE.ACESFilmicToneMapping;
+      renderer.toneMappingExposure = 1;
       renderer.setSize(sizes.width, sizes.height);
       renderer.setPixelRatio(sizes.pixelRatio);
-      renderer.setClearColor(debugObject.clearColor);
-
-      /**
-       * Base geometry
-       */
-
-      const baseGeometry = {
-        instance: new THREE.SphereGeometry(3),
-        count: 0,
-      };
-
-      baseGeometry.count = baseGeometry.instance.attributes.position.count;
-
-      /**
-       * GPU compute
-       */
-      // Setup
-      const gpgpu: {
-        size: number;
-        computation?: GPUComputationRenderer;
-        particlesVariable?: Variable;
-      } = { size: Math.ceil(Math.sqrt(baseGeometry.count)) };
-      gpgpu.computation = new GPUComputationRenderer(
-        gpgpu.size,
-        gpgpu.size,
-        renderer,
-      );
-
-      // Base particles
-
-      const baseParticlesTexture = gpgpu.computation.createTexture();
-
-      // Particles variable
-      gpgpu.particlesVariable = gpgpu.computation.addVariable(
-        "uParticles",
-        gpgpuParticlesShader,
-        baseParticlesTexture,
-      );
-
-      gpgpu.computation.setVariableDependencies(gpgpu.particlesVariable, [
-        gpgpu.particlesVariable,
-      ]);
-
-      // Init
-      gpgpu.computation.init();
-
-      /**
-       * Particles
-       */
-
-      let particles: {
-        material: THREE.ShaderMaterial;
-        points?: THREE.Points;
-      } | null = null;
-
-      particles = {
-        material: new THREE.ShaderMaterial({
-          vertexShader: particlesVertexShader,
-          fragmentShader: particlesFragmentShader,
-          uniforms: {
-            uSize: new THREE.Uniform(0.4),
-            uResolution: new THREE.Uniform(
-              new THREE.Vector2(
-                sizes.width * sizes.pixelRatio,
-                sizes.height * sizes.pixelRatio,
-              ),
-            ),
-          },
-        }),
-      };
-
-      // Points
-      particles.points = new THREE.Points(
-        baseGeometry.instance,
-        particles.material,
-      );
-      scene.add(particles.points);
-
-      /**
-       * Tweaks
-       */
-      gui.addColor(debugObject, "clearColor").onChange(() => {
-        renderer.setClearColor(debugObject.clearColor);
-      });
-      gui
-        .add(particles.material.uniforms.uSize, "value")
-        .min(0)
-        .max(1)
-        .step(0.001)
-        .name("uSize");
 
       /**
        * Animate
@@ -285,22 +322,23 @@ const WebGLTestMain = () => {
       const timer = new THREE.Timer();
       let previousTime = 0;
 
-      let currentIntersect: null | THREE.Intersection<
-        THREE.Object3D<THREE.Object3DEventMap>
-      > = null;
+      let currentIntersect: null | THREE.Intersection<THREE.Object3D<THREE.Object3DEventMap>> =
+        null;
 
       const tick = () => {
         // controls.update();
         timer.update();
 
         const elapsedTime = timer.getElapsed();
+        const deltaTime = elapsedTime - previousTime;
+        previousTime = elapsedTime;
+
+        // Materials
+
+        uniforms.uTime.value = elapsedTime;
 
         // Update controls
         controls.update();
-
-        // GPGPU Update
-
-        gpgpu.computation?.compute();
 
         // Render
         renderer.render(scene, camera);
