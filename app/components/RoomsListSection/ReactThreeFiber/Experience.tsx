bd39d5d76c34c107795721fad35d0033f1957fe0
paHa345 <pav.345@mail.ru>
2026-06-04 14:29:48 +0700

    add three js example 1.60 3d text with r3f

--- app/components/RoomsListSection/ReactThreeFiber/Experience.tsx
+++ app/components/RoomsListSection/ReactThreeFiber/Experience.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { useFrame, useLoader } from "@react-three/fiber";
-import React, { Suspense, useRef } from "react";
+import React, { Suspense, useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 
 import { DirectionalLight, Mesh, Object3DEventMap } from "three";
 import {
   AccumulativeShadows,
   BakeShadows,
+  Center,
   ContactShadows,
   Environment,
   Float,
@@ -21,8 +22,10 @@ import {
   SoftShadows,
   Stage,
   Text,
+  Text3D,
   TransformControls,
   useHelper,
+  useMatcapTexture,
 } from "@react-three/drei";
 import { Object3D } from "three";
 import { button, useControls } from "leva";
@@ -35,13 +38,37 @@ import Fox from "./Fox";
 
 // extend({ OrbitControls: OrbitControls });
 
+const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32);
+const material = new THREE.MeshMatcapMaterial();
+
 const Experience = () => {
+  const [matcap, url] = useMatcapTexture(92, 256);
+  const donutsGroup = useRef<THREE.Group | null>(null);
+
+  useEffect(() => {
+    material.matcap = matcap;
+    material.needsUpdate = true;
+  }, []);
+
+  //   const [torusGeometry, setTorusGeometry] = useState();
+  //   const [material, setMaterial] = useState();
+
+  useFrame((state, delta) => {
+    if (donutsGroup.current === null) return;
+    for (const donut of donutsGroup.current.children) {
+      donut.rotation.y += delta * 0.2;
+    }
+  });
+
   return (
     <>
       <OrbitControls makeDefault />
 
       <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} shadow-normalBias={0.04} />
       <ambientLight intensity={1.5} />
+
+      {/* <torusGeometry ref={setTorusGeometry} args={[1, 0.6, 16, 32]} />
+      <meshMatcapMaterial ref={setMaterial} matcap={matcap} /> */}
 
       {/* <mesh castShadow position-x={-2}>
         <sphereGeometry />
@@ -53,15 +80,50 @@ const Experience = () => {
         <meshStandardMaterial color="mediumpurple" />
       </mesh> */}
 
-      <mesh receiveShadow rotation-x={-Math.PI * 0.5} scale={10}>
+      {/* <mesh receiveShadow rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
-      <Suspense fallback={<Placeholder position-y={0.5} scale={[2, 3, 2]}></Placeholder>}>
-        {/* <Model></Model> */}
-        <Hamburger scale={0.35}></Hamburger>
+      <Suspense fallback={<Placeholder position-y={0.5} scale={[2, 3, 2]}></Placeholder>}> */}
+      {/* <Model></Model> */}
+      {/* <Hamburger scale={0.35}></Hamburger>
         <Fox></Fox>
-      </Suspense>
+      </Suspense> */}
+      <Center>
+        <Text3D
+          material={material}
+          font={"./fonts/helvetiker_regular.typeface.json"}
+          size={0.75}
+          height={0.2}
+          curveSegments={12}
+          bevelEnabled
+          bevelThickness={0.02}
+          bevelSize={0.02}
+          bevelOffset={0}
+          bevelSegments={5}
+        >
+          AKA 9000
+        </Text3D>
+      </Center>
+
+      <group ref={donutsGroup}>
+        {[...Array(100)].map((value, index) => {
+          return (
+            <mesh
+              key={index}
+              geometry={torusGeometry}
+              material={material}
+              position={[
+                (Math.random() - 0.5) * 10,
+                (Math.random() - 0.5) * 10,
+                (Math.random() - 0.5) * 10,
+              ]}
+              scale={0.2 + Math.random() * 0.2}
+              rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
+            ></mesh>
+          );
+        })}
+      </group>
     </>
   );
 };
