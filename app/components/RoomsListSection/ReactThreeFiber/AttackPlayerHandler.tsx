77c6f78997201d591f7cd4c4da57a089baf38e56
paHa345 <pav.345@mail.ru>
2026-08-20 17:06:13 +0700

    craft org 3d add player attack mechanism v1.1 add ray visualization

--- app/components/RoomsListSection/ReactThreeFiber/AttackPlayerHandler.tsx
+++ app/components/RoomsListSection/ReactThreeFiber/AttackPlayerHandler.tsx
@@ -9,6 +9,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import * as THREE from "three";
 import * as rapier from "@dimforge/rapier3d-compat"; // или '@rapier3d', зависит от версии
+import { Line } from "@react-three/drei";
 
 const AttackPlayerHandler = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -22,16 +23,111 @@ const AttackPlayerHandler = () => {
 
   const { world } = useRapier();
 
+  function DrawLine() {
+    if (!player) return;
+
+    const playerCenterBody = vec3({
+      x: player.translation().x,
+      y: player.translation().y,
+      z: player.translation().z,
+    });
+
+    // Поднимаем точку запуска луча на 0,5 метра вверх
+    playerCenterBody.add(new THREE.Vector3(0, 1, 0).multiplyScalar(0.5));
+
+    // Получаем угол поворота игрока
+    const playerRotation = player.rotation();
+    const quat = new THREE.Quaternion(
+      playerRotation.x,
+      playerRotation.y,
+      playerRotation.z,
+      playerRotation.w,
+    );
+
+    /**
+     * Поворачиваем этот угол поворота на 180 градусов,
+     * у нас модель изначально развернута задом наперёд
+     */
+
+    // тут угол поворота на 180 градусов по вертикали
+    const deltaQuat = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI);
+    // прибавляем 180 град по вертикали к повороту модели
+    const newQuat = quat.multiply(deltaQuat);
+
+    // Получаем направление луча, который направлен прямо от модели игрока
+    const direction = new THREE.Vector3(0, 0, -1).applyQuaternion(newQuat);
+
+    // Немного смещаем начальную точку луча, чтобы он не шёл из модели
+    // а немного перед ней
+    playerCenterBody.add(direction.clone().multiplyScalar(0.4));
+
+    const points = [
+      playerCenterBody.x,
+      playerCenterBody.y,
+      playerCenterBody.z,
+      playerCenterBody.x + direction.x * 2,
+      playerCenterBody.y + direction.y * 2,
+      playerCenterBody.z + direction.z * 2,
+    ];
+    return (
+      <Line points={points}>
+        <lineBasicMaterial color="red" linewidth={10} />
+      </Line>
+    );
+  }
+
   useEffect(() => {
     if (!player) return;
-    if (playerAttackStatus) {
-      const playerCenterBody = vec3(player.translation());
-      const playerRotation = player.rotation();
 
-      const direction = new THREE.Vector3(0, 0, -1).applyQuaternion(playerRotation);
+    /**
+     * При ударе запускается луч
+     */
+
+    if (playerAttackStatus) {
+      // Координаты игрока, центр масс
+      const playerCenterBody = vec3({
+        x: player.translation().x,
+        y: player.translation().y,
+        z: player.translation().z,
+      });
+
+      // Поднимаем точку запуска луча на 0,5 метра вверх
+      playerCenterBody.add(new THREE.Vector3(0, 1, 0).multiplyScalar(0.5));
+
+      // Получаем угол поворота игрока
+      const playerRotation = player.rotation();
+      const quat = new THREE.Quaternion(
+        playerRotation.x,
+        playerRotation.y,
+        playerRotation.z,
+        playerRotation.w,
+      );
+
+      /**
+       * Поворачиваем этот угол поворота на 180 градусов,
+       * у нас модель изначально развернута задом наперёд
+       */
+
+      // тут угол поворота на 180 градусов по вертикали
+      const deltaQuat = new THREE.Quaternion().setFromAxisAngle(
+        new THREE.Vector3(0, 1, 0),
+        Math.PI,
+      );
+      // прибавляем 180 град по вертикали к повороту модели
+      const newQuat = quat.multiply(deltaQuat);
+
+      // Получаем направление луча, который направлен прямо от модели игрока
+      const direction = new THREE.Vector3(0, 0, -1).applyQuaternion(newQuat);
+
+      // Немного смещаем начальную точку луча, чтобы он не шёл из модели
+      // а немного перед ней
+      playerCenterBody.add(direction.clone().multiplyScalar(0.4));
+
+      // Получаем луч, который идёт из вычисленной точки по заданному направлению вперёд
       const ray = new rapier.Ray(playerCenterBody, direction);
 
-      const hit = world.castRay(ray, 0.15, true);
+      // Запускаем этот луч с установленной длинной
+      const hit = world.castRay(ray, 2, false);
       console.log(hit);
 
       setTimeout(() => {
@@ -40,7 +136,11 @@ const AttackPlayerHandler = () => {
     }
   }, [playerAttackStatus]);
 
-  return <></>;
+  return (
+    <>
+      <DrawLine></DrawLine>
+    </>
+  );
 };
 
 export default AttackPlayerHandler;
