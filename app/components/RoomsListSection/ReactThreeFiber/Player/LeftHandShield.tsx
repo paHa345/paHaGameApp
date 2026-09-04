d9ae1f9d8d27eb252779fa654ba919bd2607bef5
paHa345 <pav.345@mail.ru>
2026-09-04 15:57:02 +0700

    craft org 3d add fbx players shield component v1.4

--- app/components/RoomsListSection/ReactThreeFiber/Player/LeftHandShield.tsx
+++ app/components/RoomsListSection/ReactThreeFiber/Player/LeftHandShield.tsx
@@ -8,32 +8,32 @@ const LeftHandShield = () => {
   const meshRef = useRef<THREE.Mesh>(null);
   console.log(model);
   const mesh = model.children[0].clone() as THREE.Mesh;
-  const clonedShield = useMemo(() => {
-    // Применяем матрицу трансформации к геометрии
+
+  useEffect(() => {
+    if (!model || !model.children[0]) return;
+
+    // Очищаем старую геометрию
+    if (meshRef.current) {
+      meshRef.current.geometry.dispose();
+    }
+
+    const mesh = model.children[0].clone() as THREE.Mesh;
+    mesh.geometry = mesh.geometry.clone();
     mesh.geometry.applyMatrix4(model.children[0].matrixWorld);
-    return mesh;
+    mesh.matrix.identity();
+
+    meshRef.current = mesh;
   }, [model]);
 
-  //   useEffect(() => {
+  //   const clonedShield = useMemo(() => {
   //     // Применяем матрицу трансформации к геометрии
   //     mesh.geometry.applyMatrix4(model.children[0].matrixWorld);
+  //     return mesh;
   //   }, [model]);
-
-  //   useEffect(() => {
-  //     if (!model || !model.children[0]) return;
-
-  //     // Очищаем старую геометрию
-  //     if (meshRef.current) {
-  //       meshRef.current.geometry.dispose();
-  //     }
-
-  //     const mesh = model.children[0].clone() as THREE.Mesh;
-  //     mesh.geometry = mesh.geometry.clone();
-  //     mesh.geometry.applyMatrix4(model.children[0].matrixWorld);
-  //     mesh.matrix.identity();
-
-  //     meshRef.current = mesh;
-  //   }, [model]);
+  useEffect(() => {
+    // Применяем матрицу трансформации к геометрии
+    mesh.geometry.applyMatrix4(model.children[0].matrixWorld);
+  }, [model]);
 
   const material = new THREE.MeshStandardMaterial({
     map: colorMap,
@@ -43,7 +43,7 @@ const LeftHandShield = () => {
     <>
       <primitive
         material={material}
-        object={clonedShield}
+        object={mesh}
         scale={1.6}
         position={[0.48, -1.1, 0]}
         rotation-x={-Math.PI / 2}
