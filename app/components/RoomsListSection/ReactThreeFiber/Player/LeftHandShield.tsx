b3a01dff17e1cc7983b5b7fb8e3be9188418a2cb
paHa345 <pav.345@mail.ru>
2026-09-04 15:48:12 +0700

    craft org 3d add fbx players shield component v1.3

--- app/components/RoomsListSection/ReactThreeFiber/Player/LeftHandShield.tsx
+++ app/components/RoomsListSection/ReactThreeFiber/Player/LeftHandShield.tsx
@@ -3,37 +3,37 @@ import React, { useEffect, useMemo, useRef } from "react";
 import * as THREE from "three";
 
 const LeftHandShield = () => {
-  const model = useFBX("./models/Shields/shield_1.fbx");
+  const model = useFBX("./models/Shields/shield_20.fbx");
   const [colorMap] = useTexture(["./models/Shields/Texture_MAp_shields.png"]);
   const meshRef = useRef<THREE.Mesh>(null);
   console.log(model);
   const mesh = model.children[0].clone() as THREE.Mesh;
-  //   const clonedShield = useMemo(() => {
-  //     // Применяем матрицу трансформации к геометрии
-  //     mesh.geometry.applyMatrix4(model.children[0].matrixWorld);
-  //     return mesh;
-  //   }, [model]);
-
-  useEffect(() => {
+  const clonedShield = useMemo(() => {
     // Применяем матрицу трансформации к геометрии
     mesh.geometry.applyMatrix4(model.children[0].matrixWorld);
+    return mesh;
   }, [model]);
 
-  useEffect(() => {
-    if (!model || !model.children[0]) return;
+  //   useEffect(() => {
+  //     // Применяем матрицу трансформации к геометрии
+  //     mesh.geometry.applyMatrix4(model.children[0].matrixWorld);
+  //   }, [model]);
 
-    // Очищаем старую геометрию
-    if (meshRef.current) {
-      meshRef.current.geometry.dispose();
-    }
+  //   useEffect(() => {
+  //     if (!model || !model.children[0]) return;
 
-    const mesh = model.children[0].clone() as THREE.Mesh;
-    mesh.geometry = mesh.geometry.clone();
-    mesh.geometry.applyMatrix4(model.children[0].matrixWorld);
-    mesh.matrix.identity();
+  //     // Очищаем старую геометрию
+  //     if (meshRef.current) {
+  //       meshRef.current.geometry.dispose();
+  //     }
 
-    meshRef.current = mesh;
-  }, [model]);
+  //     const mesh = model.children[0].clone() as THREE.Mesh;
+  //     mesh.geometry = mesh.geometry.clone();
+  //     mesh.geometry.applyMatrix4(model.children[0].matrixWorld);
+  //     mesh.matrix.identity();
+
+  //     meshRef.current = mesh;
+  //   }, [model]);
 
   const material = new THREE.MeshStandardMaterial({
     map: colorMap,
@@ -43,7 +43,7 @@ const LeftHandShield = () => {
     <>
       <primitive
         material={material}
-        object={mesh}
+        object={clonedShield}
         scale={1.6}
         position={[0.48, -1.1, 0]}
         rotation-x={-Math.PI / 2}
